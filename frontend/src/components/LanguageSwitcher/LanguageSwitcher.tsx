47837336009501dcf2a38838af1d9c190f7e0ca5
Susanna Andreeva <sanadex90@gmail.com>
2025-01-20 11:48:38 +0600

    fix(language-switcher): close dropdown on outside click

--- frontend/src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ frontend/src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -3,12 +3,19 @@ import { useLanguage } from '../context/LanguageContext.tsx';
 import { MdLanguage } from 'react-icons/md';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import './LanguageSwitcher.css';
+import { useOutsideClick } from '../../app/hooks/useOutsideClick.ts';
 
 const LanguageSwitcher = () => {
   const { language, setLanguage } = useLanguage();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
+
+  const closeDropdown = () => setIsDropdownOpen(false);
+
+  const ref = useOutsideClick(closeDropdown);
+
   const toggleLanguage = (lang: 'en-us' | 'ru') => {
+    console.log('Changing language to:', lang);
     setLanguage(lang);
     setIsDropdownOpen(false);
   };
@@ -18,24 +25,28 @@ const LanguageSwitcher = () => {
   };
 
   return (
-    <div className="language-switcher">
-      <div onClick={toggleDropdown} className="language-switcher__button">
-        <MdLanguage className="language-switcher__icon" />
-        <span className="language-switcher__text">{language === 'en-us' ? 'EN' : 'RU'}</span>
-        <MdKeyboardArrowDown className="language-switcher__arrow" />
+    <div ref={ref} className="language-switcher">
+      <div className="language-switcher">
+        <div onClick={toggleDropdown} className="language-switcher__button">
+          <MdLanguage className="language-switcher__icon" />
+          <span className="language-switcher__text">{language === 'en-us' ? 'EN' : 'RU'}</span>
+          <MdKeyboardArrowDown className="language-switcher__arrow" />
+        </div>
+
+        {isDropdownOpen && (
+          <div className="language-switcher__dropdown">
+            <button className="language-switcher__option" onClick={() => toggleLanguage('ru')}>
+              RU
+            </button>
+            <button className="language-switcher__option" onClick={() => toggleLanguage('en-us')}>
+              EN
+            </button>
+          </div>
+        )}
       </div>
 
-      {isDropdownOpen && (
-        <div className="language-switcher__dropdown">
-          <button className="language-switcher__option" onClick={() => toggleLanguage('ru')}>
-            RU
-          </button>
-          <button className="language-switcher__option" onClick={() => toggleLanguage('en-us')}>
-            EN
-          </button>
-        </div>
-      )}
     </div>
+
   );
 };
 
