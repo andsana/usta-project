ea591d495a2dbb04b5cd8804ecb23613fca0e83d
Susanna Andreeva <sanadex90@gmail.com>
2025-02-03 20:23:50 +0600

    feat(Banner): add scroll functionality to Banner leading to About Us section

--- frontend/src/components/Banner/Banner.tsx
+++ frontend/src/components/Banner/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 import './Banner.css';
 import { useScreenDetector } from '../../app/hooks/useScreenDetector.ts';
-
+import { MdKeyboardArrowDown } from 'react-icons/md';
 
 interface BannerSliceProps {
   primary: {
@@ -33,30 +33,35 @@ const Banner: React.FC<BannerProps> = ({ slice }) => {
     : { backgroundImage: `url(${slice.primary.image.url})` };
 
   return (
-        <div className="banner"
-             style={bannerStyle}
-        >
-          {slice.primary.video.url && !isMobile && (
-            <video
-              className="banner-video"
-              src={slice.primary.video.url}
-              autoPlay
-              loop
-              muted
-            />
-          )}
-          <div className="banner-container">
-            <div className="banner__content">
-              <div className="banner__content-col">
-                <h1 className="banner__content-title">{slice.primary.slogan}</h1>
-                <h4 className="banner__content-subtitle">{slice.primary.slogandescription}</h4>
-                {/*<Link to={slice.primary.buttonlink} className="banner__content-button">*/}
-                {/*  {slice.primary.buttonname}*/}
-                {/*</Link>*/}
-              </div>
+    <div className="banner"
+         style={bannerStyle}
+    >
+      {slice.primary.video.url && !isMobile && (
+        <video
+          className="banner-video"
+          src={slice.primary.video.url}
+          autoPlay
+          loop
+          muted
+        />
+      )}
+      <div className="banner-container">
+        <div className="banner__content">
+          <div className="banner__content-col">
+            <h1 className="banner__content-title">{slice.primary.slogan}</h1>
+            <div className="banner__content-subtitle-wrapper">
+              <h4 className="banner__content-subtitle">{slice.primary.slogandescription}</h4>
+              <a className="banner__scroll-top" href="#whoAreWe">
+                <MdKeyboardArrowDown className="banner__arrow-top" />
+              </a>
             </div>
+            {/*<Link to={slice.primary.buttonlink} className="banner__content-button">*/}
+            {/*  {slice.primary.buttonname}*/}
+            {/*</Link>*/}
           </div>
         </div>
+      </div>
+    </div>
   );
 };
 
