bbde7749ed9095d7dab2d981f8ad874ec7be795f
Susanna Andreeva <sanadex90@gmail.com>
2025-02-03 22:58:09 +0600

    feat(ProjectCard): add project link with category and slug, transliteration for Russian language, and lowercase URL formatting

--- frontend/src/components/Projects/ProjectCard/ProjectCard.tsx
+++ frontend/src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { HiOutlineArrowRight } from 'react-icons/hi';
+import React, { useContext } from 'react';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+import MyLink from '../../MyLink/MyLink.tsx';
+import { transliterate } from '../../../app/utils/transliterate.ts';
 import './ProjectCard.css';
+import { LanguageContext } from '../../../app/contexts/LanguageContext.tsx';
 
 export interface Card {
   title: string;
@@ -14,14 +17,28 @@ interface ProjectCardProps {
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
+  const languageContext = useContext(LanguageContext);
+
+  if (!languageContext) {
+    throw new Error('LanguageContext must be used within a LanguageProvider');
+  }
+
+  const { language } = languageContext;
+
+  const slugTitle = language === 'ru' ? transliterate(project.title).replace(/\s|,/g, '-').toLowerCase() : project.title.replace(/\s|,/g, '-').toLowerCase();
+  const slugLocation = language === 'ru' ? transliterate(project.location).replace(/\s|,/g, '-').toLowerCase() : project.location.replace(/\s|,/g, '-').toLowerCase();
+  const slugCategory = language === 'ru' ? transliterate(project.category).replace(/\s|,/g, '-').toLowerCase() : project.category.replace(/\s|,/g, '-').toLowerCase();
+
+  const slug = `${slugTitle}-${slugLocation}`;
+
   return (
-    <div key={`${project.title}-${project.location}`} className="project-card">
+    <MyLink className="project-card" to={`/projects/${slugCategory}/${slug}`} key={slug}>
       <div className="project-card__image-wrapper">
         <img src={project.image.url} alt={project.title} className="project-card__image" />
       </div>
       <div className="project-card__title-wrapper">
         <div className="project-card__icon-wrapper">
-          <HiOutlineArrowRight className="project-card__icon-arrow" />
+          <MdKeyboardArrowRight className="project-card__icon-arrow" />
         </div>
         <h4 className="project-card__title">{project.title}</h4>
       </div>
@@ -30,7 +47,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
         <span className="project-card__separator"></span>
         <span className="project-card__category">{project.category}</span>
       </div>
-    </div>
+    </MyLink>
   );
 };
 
