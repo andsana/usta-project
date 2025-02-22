38fc5b7268b8255f6b61ab4e8dfae8315f323f7b
Susanna Andreeva <sanadex90@gmail.com>
2025-02-22 19:53:18 +0600

    refactor(ProjectDetailPage): simplify project data fetching and state handling

--- frontend/src/components/ProjectDetailPage/ProjectDetailPage.tsx
+++ frontend/src/components/ProjectDetailPage/ProjectDetailPage.tsx
@@ -5,7 +5,6 @@ import { useLanguage } from '../../app/hooks/useLanguage.ts';
 import MyLink from '../MyLink/MyLink.tsx';
 import './ProjectDetailPage.css';
 import { translations } from '../../app/constants/translations.ts';
-import { useEffect, useState } from 'react';
 import NoPageMessage from '../NoPageMessage/NoPageMessage.tsx';
 
 interface ProjectCard {
@@ -53,7 +52,6 @@ interface ProjectDetailPageData {
   preview?: string;
   client?: string;
   ourrole?: string;
-  // location?: string;
   area?: string;
   sdg: SdgItem[];
   body: Slice[];
@@ -68,11 +66,6 @@ const ProjectDetailPage = () => {
   const { uid } = useParams();
   const location = useLocation();
 
-  // Состояние для проекта (из стейта или из Prismic)
-  const [project, setProject] = useState<ProjectCard | null>(
-    location.state.projectData || null,
-  );
-
   // Состояние для карточек проекта
   const [projectCardsDocument, { state: projectCardsState }] =
     usePrismicDocumentByUID<ProjectCardsDocument>(
@@ -83,30 +76,15 @@ const ProjectDetailPage = () => {
       },
     );
 
-  console.log('projectCardsDocument', projectCardsDocument);
+  const projectStateData: ProjectCard = location.state?.projectData;
 
-  useEffect(() => {
-    if (!project && projectCardsDocument) {
-      const projectData = projectCardsDocument.data.body
-        .flatMap((slice) => slice.items)
-        .find((item) => item.projectdetailuid === uid);
+  // Данные для проекта из документа
+  const projectData = projectCardsDocument?.data.body
+    .flatMap((slice) => slice.items)
+    .find((item) => item.projectdetailuid === uid);
 
-      if (projectData) {
-        setProject({
-          title: projectData.title,
-          location: projectData.location,
-          category: projectData.category,
-          image: {
-            url: projectData.image.url,
-            alt: projectData.image.alt || projectData.title,
-          },
-          projectdetailuid: projectData.projectdetailuid,
-        });
-      }
-
-      console.log('projectData', projectData);
-    }
-  }, [projectCardsDocument, project, uid]);
+  // Выбор данных для проекта из состояния или документа
+  const project = projectStateData || projectData;
 
   // Получаем детали проекта, если проект уже загружен
   const [projectDetailPageDocument, { state: projectDetailPageState }] =
@@ -116,9 +94,9 @@ const ProjectDetailPage = () => {
       { lang: language },
     );
 
-  // Проверяем на undefined перед использованием данных
   const projectDetailPageData = projectDetailPageDocument?.data;
 
+  // Фильтрация элементов SDG
   const filteredSdgItems = projectDetailPageData?.sdg.filter(
     (item) => item.sdgitem?.url,
   );
@@ -130,6 +108,7 @@ const ProjectDetailPage = () => {
     </div>
   );
 
+  // Показать сообщение при загрузке или ошибке
   if (projectCardsState === 'loading' || projectDetailPageState === 'loading') {
     return null;
   }
@@ -138,6 +117,7 @@ const ProjectDetailPage = () => {
     return <NoPageMessage message={translations[language].noPage} />;
   }
 
+  // Показать сообщение, если проект или данные не найдены
   if (!project || !projectDetailPageData) {
     return <NoPageMessage message={translations[language].noPage} />;
   }
@@ -239,4 +219,3 @@ const ProjectDetailPage = () => {
 };
 
 export default ProjectDetailPage;
-
