5b7125e8939170144ec4fb9031cfbc7b4e975aea
Susanna Andreeva <sanadex90@gmail.com>
2025-02-22 14:56:56 +0600

    feat(ProjectsPage): add waitForImagesToLoad and updateFavicon functions

--- frontend/src/app/constants/translations.ts
+++ frontend/src/app/constants/translations.ts
@@ -1,8 +1,10 @@
 export const translations = {
   ru: {
     noPage: 'Страница не найдена.',
+    projectsPageTitle: 'Проекты',
   },
   'en-us': {
     noPage: 'No page found.',
+    projectsPageTitle: 'Projects',
   },
 };
--- frontend/src/pages/ProjectsPage.tsx
+++ frontend/src/pages/ProjectsPage.tsx
@@ -1,32 +1,92 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { SliceZone, usePrismicDocumentByUID } from '@prismicio/react';
+
 import { useLanguage } from '../app/hooks/useLanguage.ts';
-import { useLoading } from '../app/hooks/useLoading.ts';
 import { pageComponents } from '../app/constants/pageComponents.ts';
+import { translations } from '../app/constants/translations.ts';
+
+import NoPageMessage from '../components/NoPageMessage/NoPageMessage.tsx';
+
+interface Slice {
+  slice_type: string;
+  primary: {
+    title: string;
+  };
+}
 
 const ProjectsPage = () => {
   const { language } = useLanguage();
-  const { setLoading } = useLoading();
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [document, { state }] = usePrismicDocumentByUID('page_new', 'projectcardspage', { lang: language });
+  const [page, { state }] = usePrismicDocumentByUID('page_new', 'projects', {
+    lang: language,
+  });
+
+  const updateFavicon = (isLoading: boolean) => {
+    const favicon = document.querySelector('link[rel="icon"]');
+    if (favicon) {
+      favicon.setAttribute('href', isLoading ? '/spinner.svg' : '/favicon.png');
+    }
+  };
+
+  const waitForImagesToLoad = () => {
+    const images = Array.from(document.querySelectorAll('img'));
+    const imagePromises = images.map(
+      (img) =>
+        new Promise((resolve) => {
+          if (img.complete) {
+            resolve(null);
+          } else {
+            img.onload = img.onerror = () => resolve(null);
+          }
+        }),
+    );
+
+    Promise.all(imagePromises).then(() => setIsLoading(false));
+  };
 
   useEffect(() => {
-    setLoading(state === 'loading');
-  }, [state, setLoading]);
+    setIsLoading(true);
+    updateFavicon(true);
+  }, [location.pathname]);
 
-  if (!document || !document.data) {
+  useEffect(() => {
+    if (page) {
+      const titleSlice = page.data.body.find(
+        (slice: Slice) => slice.slice_type === 'projectcardsheader',
+      );
+
+      document.title =
+        titleSlice?.primary.title || translations[language].projectsPageTitle;
+      waitForImagesToLoad();
+    }
+  }, [page, language]);
+
+  useEffect(() => {
+    updateFavicon(isLoading);
+  }, [isLoading]);
+
+  if (state === 'loading') {
     return null;
   }
 
+  if (state === 'failed') {
+    return <NoPageMessage message={translations[language].noPage} />;
+  }
+
   return (
-    <div className="projectPage__container">
-      <SliceZone
-        slices={document.data.body}
-        components={{ ...pageComponents }}
-      />
-      {/*<Projects slice={mockSlice} />*/}
-    </div>
+    <>
+      {page && (
+        <div className="projectPage__container">
+          <SliceZone
+            slices={page.data.body}
+            components={{ ...pageComponents }}
+          />
+        </div>
+      )}
+    </>
   );
 };
-
 export default ProjectsPage;
