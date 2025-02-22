110005e06eb25dfec25efb36774f731486d31a0c
Susanna Andreeva <sanadex90@gmail.com>
2025-02-22 14:56:28 +0600

    feat(ServiceDetailPage): add waitForImagesToLoad and updateFavicon functions

--- frontend/src/pages/ServiceDetailPage.tsx
+++ frontend/src/pages/ServiceDetailPage.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { SliceZone, usePrismicDocumentByUID } from '@prismicio/react';
+
 import { useLanguage } from '../app/hooks/useLanguage.ts';
+
 import { pageComponents } from '../app/constants/pageComponents.ts';
 import { translations } from '../app/constants/translations.ts';
+
 import ServiceDetailHeader from '../components/ServiceDetailHeader/ServiceDetailHeader.tsx';
 import NoPageMessage from '../components/NoPageMessage/NoPageMessage.tsx';
 
@@ -20,51 +23,49 @@ const ServiceDetailPage = () => {
     { lang: language },
   );
 
-  // Устанавливаем фавикон в спиннер при смене страницы
-  useEffect(() => {
+  // Обновление фавикона
+  const updateFavicon = (isLoading: boolean) => {
     const favicon = document.querySelector('link[rel="icon"]');
     if (favicon) {
-      favicon.setAttribute('href', '/spinner.svg');
+      favicon.setAttribute('href', isLoading ? '/spinner.svg' : '/favicon.png');
     }
+  };
+
+  // Ожидание загрузки изображений
+  const waitForImagesToLoad = () => {
+    const images = Array.from(document.querySelectorAll('img'));
+    const imagePromises = images.map(
+      (img) =>
+        new Promise((resolve) => {
+          if (img.complete) {
+            resolve(null);
+          } else {
+            img.onload = img.onerror = () => resolve(null);
+          }
+        }),
+    );
+
+    Promise.all(imagePromises).then(() => setIsLoading(false));
+  };
+
+  // Устанавливаем фавикон в спиннер при смене страницы
+  useEffect(() => {
+    updateFavicon(true);
     setIsLoading(true);
   }, [location.pathname]);
 
   useEffect(() => {
     if (page) {
       document.title = page.data.title;
-
-      const images = Array.from(document.querySelectorAll('img'));
-      const imagePromises = images.map(
-        (img) =>
-          new Promise((resolve) => {
-            if (img.complete) {
-              resolve(null);
-            } else {
-              const onLoadOrError = () => {
-                resolve(null);
-                img.onload = null;
-                img.onerror = null;
-              };
-              img.onload = onLoadOrError;
-              img.onerror = onLoadOrError;
-            }
-          }),
-      );
-
-      Promise.all(imagePromises).then(() => {
-        setIsLoading(false);
-      });
+      waitForImagesToLoad();
     }
   }, [page]);
 
   useEffect(() => {
-    const favicon = document.querySelector('link[rel="icon"]');
-    if (favicon) {
-      favicon.setAttribute('href', isLoading ? '/spinner.svg' : '/favicon.png');
-    }
+    updateFavicon(isLoading);
   }, [isLoading]);
 
-  if (state === 'loading' || isLoading) {
+  if (state === 'loading') {
     return null;
   }
 
