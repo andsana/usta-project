53e732e434ecd630ab4225aa11f7579791a61d9c
Susanna Andreeva <sanadex90@gmail.com>
2025-02-23 20:19:59 +0600

    Refactor routing and add smooth scrolling in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,52 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header.tsx';
 import HomePage from './pages/HomePage.tsx';
-import Loader from './components/Loader/Loader.tsx';
 import ProjectsPage from './pages/ProjectsPage.tsx';
-import ProjectDetail from './components/ProjectDetail/ProjectDetail.tsx';
-import Footer from './components/footer/Footer.tsx';
-import { useLoading } from './app/hooks/useLoading.ts';
-import './App.css';
+import ProjectDetailPage from './components/ProjectDetailPage/ProjectDetailPage.tsx';
 import ServiceDetailPage from './pages/ServiceDetailPage.tsx';
+import Footer from './components/Footer/Footer.tsx';
+import './App.css';
 
 const App = () => {
-  const { isLoading } = useLoading();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollTo) {
+      setTimeout(() => {
+        const element = document.getElementById(location.state.scrollTo);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 300);
+    }
+  }, [location]);
 
   return (
-    <>
-      {isLoading && <Loader />}
+    <div className="page-wrapper">
       <Header />
       <main>
         <Routes>
-          <Route path="/en" element={<HomePage />} />
           <Route path="/" element={<HomePage />} />
+          <Route path="/en" element={<HomePage />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/en/projects" element={<ProjectsPage />} />
+          <Route path="/en/projects/:category" element={<ProjectsPage />} />
+          <Route path="/projects/:category" element={<ProjectsPage />} />
           <Route
-            path="en/projects/:category/:slug"
-            element={<ProjectDetail />}
+            path="/en/projects/:category/:uid"
+            element={<ProjectDetailPage />}
+          />
+          <Route
+            path="/projects/:category/:uid"
+            element={<ProjectDetailPage />}
           />
           <Route path="/services/:uid" element={<ServiceDetailPage />} />
           <Route path="/en/services/:uid" element={<ServiceDetailPage />} />
         </Routes>
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
