e4e775cf0af6ed608287597543e1407ab5a2e213
Susanna Andreeva <sanadex90@gmail.com>
2025-02-23 20:03:31 +0600

    feat(translations): add new project-related translations for ru and en-us

--- frontend/src/app/constants/translations.ts
+++ frontend/src/app/constants/translations.ts
@@ -1,10 +1,24 @@
 export const translations = {
   ru: {
     noPage: 'Страница не найдена.',
+    noProject: 'Проекты не найден.',
+    noProjects: 'Проекты не найдены.',
     projectsPageTitle: 'Проекты',
+    location: 'Страна',
+    client: 'Клиент',
+    ourrole: 'Наша роль',
+    area: 'Площадь',
+    allProjects: 'Смотреть все',
   },
   'en-us': {
     noPage: 'No page found.',
+    noProject: 'No projects found.',
+    noProjects: 'No projects found.',
     projectsPageTitle: 'Projects',
+    location: 'Country',
+    client: 'Client',
+    ourrole: 'Our role',
+    area: 'Area',
+    allProjects: 'See all',
   },
 };
