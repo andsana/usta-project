be10cf2ba79bb11e8dfe482cff19fca853192d51
Susanna Andreeva <sanadex90@gmail.com>
2025-02-23 20:09:22 +0600

    fix(translations): correct translation for 'noProject' in Russian

--- frontend/src/app/constants/translations.ts
+++ frontend/src/app/constants/translations.ts
@@ -1,7 +1,7 @@
 export const translations = {
   ru: {
     noPage: 'Страница не найдена.',
-    noProject: 'Проекты не найден.',
+    noProject: 'Проект не найден.',
     noProjects: 'Проекты не найдены.',
     projectsPageTitle: 'Проекты',
     location: 'Страна',
@@ -12,7 +12,7 @@ export const translations = {
   },
   'en-us': {
     noPage: 'No page found.',
-    noProject: 'No projects found.',
+    noProject: 'No project found.',
     noProjects: 'No projects found.',
     projectsPageTitle: 'Projects',
     location: 'Country',
