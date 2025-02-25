6c9f37c987088448077aef800d0f2837c1d2bf93
Susanna Andreeva <sanadex90@gmail.com>
2025-02-25 23:23:10 +0600

    refactor(ServiceDetailPage): extract favicon logic and image loading to utils

--- frontend/src/pages/ServiceDetailPage.tsx
+++ frontend/src/pages/ServiceDetailPage.tsx
@@ -1,19 +1,23 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { SliceZone, usePrismicDocumentByUID } from '@prismicio/react';
-import { Helmet } from 'react-helmet-async'; // Импортируем Helmet
+import { Helmet } from 'react-helmet-async';
 import { useLanguage } from '../app/hooks/useLanguage.ts';
 import { pageComponents } from '../app/constants/pageComponents.ts';
 import { translations } from '../app/constants/translations.ts';
+import {
+  createAnimatedFavicon,
+  stopAnimatedFavicon,
+} from '../app/utils/animatedFavicon.ts';
+import { waitForImagesToLoad } from '../app/utils/waitForImagesToLoad.ts';
 import ServiceDetailHeader from '../components/ServiceDetailHeader/ServiceDetailHeader.tsx';
 import NoContentMessage from '../components/NoContentMessage/NoContentMessage.tsx';
+import Breadcrumbs from '../components/Breadcrumbs/Breadcrumbs.tsx';
 
 const ServiceDetailPage = () => {
   const { uid } = useParams();
   const { language } = useLanguage();
   const location = useLocation();
-
-  const [favicon, setFavicon] = useState('/spinner.gif');
 
   const [page, { state }] = usePrismicDocumentByUID(
     'servicedetail',
@@ -21,61 +25,52 @@ const ServiceDetailPage = () => {
     { lang: language },
   );
 
-  // Обновление фавикона
-  const updateFavicon = (isLoading: boolean) => {
-    const newFavicon = isLoading ? '/spinner.gif' : '/favicon.svg';
-    setFavicon(newFavicon); // Обновляем состояние favicon
-    const faviconElement = document.querySelector('link[rel="icon"]');
-    if (faviconElement) {
-      faviconElement.setAttribute('href', newFavicon);
-    }
-  };
+  const pageTitle: string = page
+    ? page.data.title
+    : translations[language].serviceDetailPageTitle;
 
-  const waitForImagesToLoad = useCallback(() => {
-    const images = Array.from(document.querySelectorAll('img'));
-    const imagePromises = images.map(
-      (img) =>
-        new Promise((resolve) => {
-          if (img.complete) {
-            resolve(null);
-          } else {
-            img.onload = img.onerror = () => resolve(null);
-          }
-        }),
-    );
-
-    Promise.all(imagePromises).then(() => updateFavicon(false));
-  }, []);
+  const breadcrumbs = (
+    <Breadcrumbs
+      items={[{ text: translations[language].services }]}
+      currentText={page && pageTitle}
+    />
+  );
 
   // Устанавливаем фавикон в спиннер при смене страницы
   useEffect(() => {
-    updateFavicon(true); // Показываем спиннер
+    createAnimatedFavicon();
   }, [location.pathname]);
 
   useEffect(() => {
     if (state === 'loading') {
-      updateFavicon(true);
+      createAnimatedFavicon();
     } else if (state === 'failed') {
-      updateFavicon(false);
+      stopAnimatedFavicon();
     } else if (page) {
       waitForImagesToLoad();
     }
-  }, [state, page, waitForImagesToLoad]);
+  }, [state, page]);
 
   if (state === 'loading') {
     return null;
   }
 
   if (state === 'failed') {
-    return <NoContentMessage message={translations[language].noPage} />;
+    return (
+      <>
+        {breadcrumbs}
+        <NoContentMessage message={translations[language].noServiceDetails} />
+      </>
+    );
   }
 
   return (
     <>
       <Helmet>
-        <title>{page ? page.data.title : 'Usta International'}</title>
-        <link rel="icon" href={favicon} />
+        <title>{pageTitle}</title>
       </Helmet>
+      {breadcrumbs}
+
       {page && (
         <div className="service-detail-container">
           <ServiceDetailHeader
