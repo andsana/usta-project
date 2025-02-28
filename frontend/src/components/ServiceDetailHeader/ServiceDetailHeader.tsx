43d8e52bdbafd61fce397d577c28364045918e7c
Susanna Andreeva <sanadex90@gmail.com>
2025-02-28 21:49:13 +0600

    feat(ServiceDetailHeader): redesigned for mobile-first approach

--- frontend/src/components/ServiceDetailHeader/ServiceDetailHeader.tsx
+++ frontend/src/components/ServiceDetailHeader/ServiceDetailHeader.tsx
@@ -13,19 +13,17 @@ const ServiceDetailHeader: React.FC<ServiceDetailHeaderProps> = ({
   image,
 }) => {
   return (
-    <div className="serviceDetailHeader">
-      <div className="serviceDetailHeader__bgimage-wrapper">
-        <div
-          className="serviceDetailHeader__bgimage"
-          style={{ backgroundImage: `url(${image.url})` }}
-        ></div>
+    <div className="serviceDetailHeader page">
+      <div
+        className="serviceDetailHeader__bgimage"
+        style={{ backgroundImage: `url(${image.url})` }}
+      ></div>
 
-        <div className="serviceDetailHeader__row">
-          <div className="serviceDetailHeader__col">
-            <div className="serviceDetailHeader__col-inner">
-              <h1>{title}</h1>
-              <p>{preview}</p>
-            </div>
+      <div className="serviceDetailHeader__row">
+        <div className="serviceDetailHeader__col">
+          <div className="serviceDetailHeader__col-inner">
+            <h1>{title}</h1>
+            <p>{preview}</p>
           </div>
         </div>
       </div>
