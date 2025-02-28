eb3d02a54192cb4634ba6c5fd3e357b47b786d74
Susanna Andreeva <sanadex90@gmail.com>
2025-02-28 21:38:09 +0600

    feat(WhatWeDo): implemented mobile-first approach

--- frontend/src/components/WhatWeDo/WhatWeDo.tsx
+++ frontend/src/components/WhatWeDo/WhatWeDo.tsx
@@ -8,7 +8,7 @@ interface Item {
 interface WhatWeDoSliceProps {
   primary: {
     title: string;
-    image: { url: string; };
+    image: { url: string };
     paragraphfirst: string;
     paragraphsecond: string;
     paragraphthird?: string;
@@ -21,31 +21,39 @@ interface WhatWeDoProps {
 }
 
 const WhatWeDo: React.FC<WhatWeDoProps> = ({ slice }) => {
-
   if (!slice) {
     return null;
   }
 
   return (
-    <div className="what-we-do">
-      <div className="what-we-do__container">
-        <div className="what-we-do__col-wrapper">
-          <div className="what-we-do__col">
-            <img className="what-we-do__image" src={slice.primary.image.url} alt={slice.primary.title} />
-          </div>
-          <div className="what-we-do__col">
-            <div className="what-we-do__col-content">
-              <h3 className="what-we-do__col-title">{slice.primary.title}</h3>
-              <p className="what-we-do__col-description">{slice.primary.paragraphfirst}</p>
-              <ul className="what-we-do__col-list">
-                {slice.items.map((item, index) => (
-                  <li key={index}>{item.item}</li>
-                ))}
-              </ul>
-              <p className="what-we-do__col-description">{slice.primary.paragraphsecond}</p>
-              {slice.primary.paragraphthird &&
-                <p className="what-we-do__col-description">{slice.primary.paragraphthird}</p>}
-            </div>
+    <div className="what-we-do block container">
+      <div className="what-we-do__wrapper">
+        <div className="what-we-do__col">
+          <img
+            className="what-we-do__image"
+            src={slice.primary.image.url}
+            alt={slice.primary.title}
+          />
+        </div>
+        <div className="what-we-do__col">
+          <div className="what-we-do__col-content">
+            <h3 className="what-we-do__col-title">{slice.primary.title}</h3>
+            <p className="what-we-do__col-description">
+              {slice.primary.paragraphfirst}
+            </p>
+            <ul className="what-we-do__col-list">
+              {slice.items.map((item, index) => (
+                <li key={index}>{item.item}</li>
+              ))}
+            </ul>
+            <p className="what-we-do__col-description">
+              {slice.primary.paragraphsecond}
+            </p>
+            {slice.primary.paragraphthird && (
+              <p className="what-we-do__col-description">
+                {slice.primary.paragraphthird}
+              </p>
+            )}
           </div>
         </div>
       </div>
