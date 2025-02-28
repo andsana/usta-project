88b0428937bc000d4a79afac7c58207c4e043a3b
Susanna Andreeva <sanadex90@gmail.com>
2025-02-28 14:19:36 +0600

    refactor(Footer): Reordered columns and adjusted spacing

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -8,6 +8,7 @@ import {
   createAnimatedFavicon,
   stopAnimatedFavicon,
 } from '../../app/utils/animatedFavicon.ts';
+import Logo from '../Logo/Logo.tsx';
 import MyLink from '../MyLink/MyLink.tsx';
 import MyButton from '../MyButton/MyButton.tsx';
 import SocialLinks from '../ SocialLinks/ SocialLinks.tsx';
@@ -72,53 +73,58 @@ const Footer = () => {
       <footer id="#footer" className="footer">
         <div className="footer__container">
           <div className="footer__content">
-            <MyLink className="footer__logo" to={document.data.logolink.url}>
-              <img src={document.data.logo.url} alt="Logo" />
-            </MyLink>
-            <div className="footer__content-inner">
-              {document.data.body.map((slice) => (
-                <div key={slice.id} className="footer__menu-section">
-                  <h6>{slice.primary.title}</h6>
-                  <ul>
-                    {slice.items.map((item) => (
-                      <li key={item.linkname}>
-                        {item.linkuid && item.linkuid.uid ? (
-                          <MyLink
-                            className="footer-link"
-                            to={
-                              item.linkuid.uid === 'projects'
-                                ? `/${item.linkuid.uid}`
-                                : `/services/${item.linkuid.uid}`
-                            }
-                          >
-                            {item.linkname}
-                          </MyLink>
-                        ) : item.link && item.link.url ? (
-                          <MyButton
-                            className={'footer-link'}
-                            linkName={item.linkname}
-                            linkUrl={item.link.url}
-                          />
-                        ) : null}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-              <div className="footer__contact">
-                <div className="footer__contact-social">
-                  <h6>{document.data.iconstitle}</h6>
-                  <SocialLinks />
-                </div>
-                <div className="footer__contact-mail">
-                  <strong>{document.data.mailtitle}: </strong>
-                  <a
-                    className="footer-link"
-                    href={`mailto:${document.data.emailaddress}`}
-                  >
-                    {document.data.emailaddress}
-                  </a>
-                </div>
+            {/*<MyLink className="footer__logo" to={document.data.logolink.url}>*/}
+            {/*  <img src={document.data.logo.url} alt="Logo" />*/}
+            {/*</MyLink>*/}
+            <div className="footer__column">
+              <Logo url={document.data.logolink.url} />
+            </div>
+
+            {document.data.body.map((slice) => (
+              <div
+                key={slice.id}
+                className="footer__column footer__menu-section"
+              >
+                <h6>{slice.primary.title}</h6>
+                <ul>
+                  {slice.items.map((item) => (
+                    <li key={item.linkname}>
+                      {item.linkuid && item.linkuid.uid ? (
+                        <MyLink
+                          className="footer-link"
+                          to={
+                            item.linkuid.uid === 'projects'
+                              ? `/${item.linkuid.uid}`
+                              : `/services/${item.linkuid.uid}`
+                          }
+                        >
+                          {item.linkname}
+                        </MyLink>
+                      ) : item.link && item.link.url ? (
+                        <MyButton
+                          className={'footer-link'}
+                          linkName={item.linkname}
+                          linkUrl={item.link.url}
+                        />
+                      ) : null}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+            <div className="footer__column footer__contact">
+              <h6>{document.data.iconstitle}</h6>
+              <div className="footer__social-links">
+                <SocialLinks />
+              </div>
+              <div className="footer__contact-mail">
+                <strong>{document.data.mailtitle}: </strong>
+                <a
+                  className="footer-link"
+                  href={`mailto:${document.data.emailaddress}`}
+                >
+                  {document.data.emailaddress}
+                </a>
               </div>
             </div>
           </div>
