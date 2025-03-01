762a08cdb697603c93db2a0f3df7092b43231451
Susanna Andreeva <sanadex90@gmail.com>
2025-03-01 18:36:54 +0600

    feat(logo): scroll to top on click

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLanguage } from '../../app/hooks/useLanguage.ts';
 import './Logo.css';
 
 interface LogoProps {
@@ -7,8 +8,23 @@ interface LogoProps {
 }
 
 const Logo: React.FC<LogoProps> = ({ url }) => {
+  const { language } = useLanguage();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const homePath = language === 'en-us' ? '/en' : '/';
+
+  const handleClick = (e: React.MouseEvent) => {
+    if (location.pathname === homePath) {
+      window.scrollTo({ top: 0, behavior: 'auto' });
+    } else {
+      e.preventDefault();
+      navigate(homePath);
+    }
+  };
+
   return (
-    <Link className="logo" to={url}>
+    <Link to={url} className="logo" onClick={handleClick}>
       <span className="logo__usta">usta</span>
       <span className="logo__international">international</span>
     </Link>
