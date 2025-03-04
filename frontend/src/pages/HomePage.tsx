a2b0d2f854e7cd8683eba30bdeda4c013265b4c7
Susanna Andreeva <sanadex90@gmail.com>
2025-03-04 17:31:52 +0600

    feat(HomePage): replace NoContentMessage with ErrorPage component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,9 @@
 import { useEffect } from 'react';
 import { SliceZone, usePrismicDocumentByUID } from '@prismicio/react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { pageComponents } from '../app/constants/pageComponents.ts';
 import { useLanguage } from '../app/hooks/useLanguage.ts';
-import NoContentMessage from '../components/NoContentMessage/NoContentMessage.tsx';
 import { translations } from '../app/constants/translations.ts';
 import {
   createAnimatedFavicon,
@@ -15,10 +14,14 @@ import { waitForImagesToLoad } from '../app/utils/waitForImagesToLoad.ts';
 const HomePage = () => {
   const { language } = useLanguage();
   const location = useLocation();
+  const navigate = useNavigate();
 
   const [page, { state }] = usePrismicDocumentByUID('page_new', 'home', {
     lang: language,
   });
+
+  const errorPageUrl = `/${language === 'en-us' ? 'en/' : ''}404`;
+  const currentUrl = `https://ustainternational.com/${language === 'en-us' ? '/en/' : ''}`;
 
   useEffect(() => {
     createAnimatedFavicon();
@@ -39,17 +42,38 @@ const HomePage = () => {
   }
 
   if (state === 'failed') {
-    return <NoContentMessage message={translations[language].noPage} />;
+    navigate(errorPageUrl);
+    return null;
+  }
+
+  if (!page) {
+    navigate(errorPageUrl);
+    return null;
   }
 
   return (
     <>
       <Helmet>
-        <title>Usta International</title>
+        <title>{translations[language].homeTitle}</title>
+        <meta
+          name="description"
+          content={translations[language].homeDescription}
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:url" content={currentUrl} />
+        <meta property="og:title" content={translations[language].homeTitle} />
+        <meta
+          property="og:description"
+          content={translations[language].homeDescription}
+        />
+        <meta
+          property="og:image"
+          content="/assets/images/ustainternational.jpg"
+        />
+        <link rel="canonical" href={currentUrl} />
+        <meta name="language" content={language} />
       </Helmet>
-      {page && (
-        <SliceZone slices={page.data.body} components={{ ...pageComponents }} />
-      )}
+      <SliceZone slices={page.data.body} components={{ ...pageComponents }} />
     </>
   );
 };
