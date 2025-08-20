beb0b4fbe34358a56f7931f4c6d71041f52691b3
Susanna Andreeva <sanadex90@gmail.com>
2025-08-20 13:18:24 +0600

    fix(ServiceDetailHeader): replaced deprecated frameBorder with CSS border in iframe

--- frontend/src/components/ServiceDetailHeader/ServiceDetailHeader.tsx
+++ frontend/src/components/ServiceDetailHeader/ServiceDetailHeader.tsx
@@ -61,7 +61,7 @@ const ServiceDetailHeader: React.FC<ServiceDetailHeaderProps> = ({
           <iframe
             src={youtube.embed_url.replace('watch?v=', 'embed/')}
             title="YouTube video"
-            frameBorder="0"
+            style={{ border: 0 }}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           />
